5b2c48fb6f75191cdb08668e443edd5535c798a3
SeolHun <shun10114@gmail.com>
2018-02-07 23:04:04 +0900

    Updated marked option but not working

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,34 +1,30 @@
-const path = require('path')
-const utils = require('./utils')
-const config = require('../config')
+const path = require('path');
+const utils = require('./utils');
+const config = require('../config');
 
-const marked = require("marked");
-const highlightjs = require("highlight.js");
-const renderer = new marked.Renderer();
-marked.setOptions({
-  highlight: function (code, lang, callback) {
-    require('pygmentize-bundled')({ lang: lang, format: 'html' }, code, function (err, result) {
-      callback(err, result.toString());
-    });
-  }
-});
+var marked = require('marked');
+var hljs = require('highlight.js');
+var renderer = new marked.Renderer();
+renderer.code = function (code, language) {
+  return `<pre><code class="hljs ${language}>${hljs.highlight(language, code).value}</code></pre>`;
+};
 
 function resolve(dir) {
-  return path.join(__dirname, '..', dir)
+  return path.join(__dirname, '..', dir);
 }
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: resolve('./src/main.tsx')
+    main: resolve('./src/main.tsx'),
   },
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+      : config.dev.assetsPublicPath,
   },
   resolve: {
     extensions: ['.js', '.ts', '.tsx', 'scss'],
@@ -45,37 +41,37 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test')],
       }, {
         test: /\.(tsx|ts)?$/,
         loader: 'awesome-typescript-loader',
         include: [resolve('src'), resolve('test')],
-        exclude: /node_modules/
+        exclude: /node_modules/,
       }, {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       }, {
         test: /\.scss$/,
         use: [{
-          loader: 'style-loader'
+          loader: 'style-loader',
         }, {
           loader: 'css-loader',
           options: {
-            sourceMap: true
-          }
+            sourceMap: true,
+          },
         }, {
           loader: 'sass-loader',
           options: {
             sourceMap: true,
-          }
-        }]
+          },
+        }],
       }, {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
+        },
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
@@ -83,7 +79,7 @@ module.exports = {
         options: {
           limit: 10000,
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
-        }
+        },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
@@ -91,26 +87,34 @@ module.exports = {
         options: {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
+        },
       }, {
         test: /\.md$/,
         use: [
           {
-            loader: "html-loader"
+            loader: 'html-loader',
           },
           {
-            loader: "markdown-loader",
+            loader: 'markdown-loader',
             options: {
               pedantic: true,
-              renderer
-            }
-          }
-        ]
-      }]
+              gfm: true,
+              tables: true,
+              breaks: false,
+              pedantic: false,
+              sanitize: false,
+              smartLists: true,
+              smartypants: false,
+              xhtml: false,
+              renderer,
+            },
+          },
+        ],
+      }],
   },
   plugins: [
     new ExtractTextPlugin({
-      filename: '[name].css'
-    })
-  ]
-}
+      filename: '[name].css',
+    }),
+  ],
+};
