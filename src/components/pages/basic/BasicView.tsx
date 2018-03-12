b83421a2b3eb0a33aa733dec1c9a2cb90d9f705d
Seolhun <shun10114@gmail.com>
2018-03-12 17:19:37 +0900

    Updated Promise Contetns

--- src/components/pages/basic/BasicView.tsx
+++ src/components/pages/basic/BasicView.tsx
@@ -1,7 +1,28 @@
 
 import * as React from 'react';
 
-class GrammarView extends React.Component {
+import * as Promise from 'promise';
+
+import { Button } from 'reactstrap';
+
+import { PromiseTest } from './examples/Promise';
+
+export interface BasicViewProps {
+
+}
+
+export interface BasicViewStates {
+
+}
+
+class BasicView extends React.Component<BasicViewProps, BasicViewStates> {
+  promise_test: PromiseTest;
+
+  constructor() {
+    super({}, {});
+    this.promise_test = new PromiseTest();
+  }
+
   render() {
     return (
       <div className='container'>
@@ -10,9 +31,45 @@ class GrammarView extends React.Component {
             <h1>BasicView</h1>
           </div>
         </div>
+        <div className='row'>
+          <div className='col-sm-3'>
+            <Button onClick={() => this.promise()} color='primary'>Promise</Button>
+          </div>
+          <div className='col-sm-3'>
+            <Button onClick={() => this.nonPromise()} color='warning'>Non-Promise</Button>
+          </div>
+          <div className='col-sm-3'>
+            <Button onClick={() => this.promiseAll()} color='success'>Promise-All</Button>
+          </div>
+          <div className='col-sm-3'>
+            <Button onClick={() => this.promiseRace()} color='info'>Promise-Race</Button>
+          </div>
+        </div>
       </div>
     );
   }
+
+  promise() {
+    // Object { name: "아반떼", doors: 4 }
+    this.promise_test.promiseCreateCar('아반떼', 4).then((car) => {
+      console.log(car);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  nonPromise() {
+    // undefined
+    const car = this.promise_test.nonPromiseCreateCar('벨로스터', 3);
+  }
+
+  promiseAll() {
+    this.promise_test.promiseAll();
+  }
+
+  promiseRace() {
+    this.promise_test.promiseRace();
+  }
 }
 
-export default GrammarView;
+export default BasicView;
---
+++ src/components/pages/basic/examples/Promise.ts
@@ -0,0 +1,95 @@
+import * as Promise from 'promise';
+
+class Car {
+  private name: string;
+  private doors: number;
+
+  constructor(name: string, doors: number) {
+    this.name = name;
+    this.doors = doors;
+  }
+}
+
+class PromiseTest {
+  promiseCreateCar(name: string, doors: number): Promise<Car> {
+    const promised_car = new Promise<Car>((resolve, reject) => {
+      try {
+        let car;
+        setTimeout(() => {
+          resolve(car = new Car(name, doors));
+        }, 1000);
+      } catch (error) {
+        reject(Error(error));
+      }
+    });
+    return promised_car;
+  }
+
+  promiseCreateCarFail(name: string, doors: number): Promise<Car> {
+    const promised_car = new Promise<Car>((resolve, reject) => {
+      try {
+        let car;
+        setTimeout(() => {
+          reject(car = new Car(name, doors));
+        }, 1500);
+      } catch (error) {
+        reject(Error(error));
+      }
+    });
+    return promised_car;
+  }
+
+  private getEvenValue(): Promise<number> {
+    return new Promise<number>((resolve, reject) => {
+      try {
+        const random = Math.ceil(Math.random() * 10);
+        console.log(random);
+        setTimeout(() => {
+          if (random % 2 === 0) {
+            resolve(random);
+          } else {
+            reject(random);
+          }
+        }, 300);
+      } catch (error) {
+        reject(Error(error));
+      }
+    });
+  }
+
+  promiseAll() {
+    const getEven = this.getEvenValue();
+    const getEven2 = this.getEvenValue();
+
+    Promise.all([getEven, getEven2]).then((car) => {
+      console.log('Promise All success', car);
+    }).catch((error) => {
+      console.log('Promise All error', error);
+    });
+  }
+
+  promiseRace() {
+    const getEven = this.getEvenValue();
+    const getEven2 = this.getEvenValue();
+
+    Promise.race([getEven, getEven2]).then((car) => {
+      console.log('Promise Race success', car);
+    }).catch((error) => {
+      console.log('Promise Race error', error);
+    });
+  }
+
+  nonPromiseCreateCar(name: string, doors: number): Car {
+    let car;
+    try {
+      setTimeout(() => {
+        car = new Car(name, doors);
+      }, 3000);
+    } catch (error) {
+      throw new Error(error);
+    }
+    return car;
+  }
+}
+
+export { Car, PromiseTest };
