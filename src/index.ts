96af05e276f2e2f28349bccee971be42834c94a6
Seolhun <shun10114@gmail.com>
2018-08-11 14:26:49 +0900

    [Components] Updated Components using index

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import * as React from 'react';
 import Alert, { AlertProps } from './components/alert/Alert';
 import Button, { ButtonProps } from './components/button/Button';
 import Hr, { HrProps } from './components/hr/Hr';
+import Chip, { ChipProps } from './components/chip/Chip';
 
-export { Button, ButtonProps, Alert, AlertProps, Hr, HrProps };
+export { Button, ButtonProps, Alert, AlertProps, Hr, HrProps, Chip, ChipProps };
+
 export default {
   React,
-  Button,
+
   Alert,
+  Button,
+  Chip,
   Hr,
 };
