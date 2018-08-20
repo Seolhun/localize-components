7e5fa6d819ba605bafc13be0d468f9d74bfa7d37
Hun, Seol(Sol) <shun10114@gmail.com>
2018-08-21 02:50:27 +0900

    [1.0.0] Fix the conflict to merge (#8)
    * v1.0.0
    
    * [docs] Updated package and divided components
    
    * [components] divided docs and components removing build, dev config
    
    * [docs] Updated Docs Build and Dev config
    
    * [Add] lerna to test binding packages and removed unnecessary packages
    
    * [Package] Separate docs and components to reduce size (#1)
    
    * [Package] Updated Package Description
    
    * [TsLint] Removed others just using airbnb
    
    * [Components] Added Component about Chips
    
    * [Components] Updated Components using index
    
    * [React] Devided package json with Docs and Components
    
    * [Package] Managing Packages deviding docs and components
    
    * v1.0.0
    
    * [docs] Updated package and divided components
    
    * [components] divided docs and components removing build, dev config
    
    * [docs] Updated Docs Build and Dev config
    
    * [Add] lerna to test binding packages and removed unnecessary packages
    
    * Add renovate.json (#3)
    
    * Add Codacy badge (#2)
    
    * [package] Add Jest package

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-# React Simple Components to customizing
+# Localize React Components
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/2f4e28800ffd40f49437308309cbae45)](https://app.codacy.com/app/shun10114/localize-react-components?utm_source=github.com&utm_medium=referral&utm_content=Seolhun/localize-react-components&utm_campaign=badger)
+[![codebeat badge](https://codebeat.co/badges/5f191928-a3e9-469d-beed-445bb4cde65d)](https://codebeat.co/projects/github-com-seolhun-localize-react-components-master)
 
 - Author : [HunSeol](https://github.com/Seolhun/)
 
 ## Used stacks
-
 - React
 - TypeScript
 - SCSS
+- Storybook
 
 ## Getting start
+#### Install Package
+- yarn add @seolhun/localize-react-components
 
-1.  yarn install
-2.  yarn dev
+#### Run Docs
+- yarn run dev
--- docs/build/webpack.dev.conf.js
+++ docs/build/webpack.dev.conf.js
@@ -11,7 +11,7 @@ function resolve(dir) {
   return path.join(__dirname, '..', dir);
 }
 
-Object.keys(baseWebpackConfig.entry).forEach(function(name) {
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
   baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(
     baseWebpackConfig.entry[name],
   );
@@ -19,7 +19,9 @@ Object.keys(baseWebpackConfig.entry).forEach(function(name) {
 
 module.exports = merge(baseWebpackConfig, {
   module: {
-    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap }),
+    rules: utils.styleLoaders({
+      sourceMap: config.dev.cssSourceMap
+    }),
   },
   devtool: '#cheap-module-eval-source-map',
   plugins: [
@@ -30,7 +32,7 @@ module.exports = merge(baseWebpackConfig, {
     new webpack.NoEmitOnErrorsPlugin(),
     new HtmlWebpackPlugin({
       filename: './index.html',
-      template: resolve("docs/index.html"),
+      template: resolve("./index.html"),
       inject: true,
     }),
     new FriendlyErrorsPlugin(),
