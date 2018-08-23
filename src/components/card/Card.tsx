1e5986b2d213a59476921084067a2e24dca237fa
Seolhun <shun10114@gmail.com>
2018-08-23 19:52:37 +0900

    [component] Card is added

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+import styles from './Card.scss';
+
+export interface CardProps {
+  children: React.ReactNode;
+  // IsNotRequired
+  className?: string;
+  style?: {
+    color?: string;
+    backgroundColor?: string;
+    padding?: string;
+    margin?: string;
+  };
+}
+
+const Card: React.StatelessComponent<CardProps> = ({
+  className,
+  children,
+  style,
+}) => (
+  <div className={`${styles.card} ${className}`} style={style}>
+    {children}
+  </div>
+);
+
+Card.defaultProps = {
+  className: '',
+  style: {
+    color: '',
+    backgroundColor: '',
+    padding: '',
+    margin: '',
+  },
+};
+
+export default Card;
