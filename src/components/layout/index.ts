d0d83086da28a997106110dd7296bb953edb368f
Seolhun <shun10114@gmail.com>
2018-08-24 12:57:55 +0900

    v1.0.8

---
+++ src/components/layout/index.ts
@@ -0,0 +1,11 @@
+import NavBar from './NavBar';
+import Footer from './Footer';
+import Sidebar from './Sidebar';
+
+export { NavBar, Footer, Sidebar };
+
+export default {
+  NavBar,
+  Footer,
+  Sidebar,
+};
