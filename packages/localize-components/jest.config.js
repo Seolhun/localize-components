676c7796c2340783c589393f1121531599e9a94b
Seolhun <shun10114@gmail.com>
2018-08-31 01:41:23 +0900

    [core] move to src

--- packages/localize-components/jest.config.js
+++ packages/localize-components/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/packages/localize-components/src'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.ts?$': 'ts-jest',
