480f429be50d734351c484a37c7f994047db5bce
Seolhun <shun10114@gmail.com>
2018-09-29 15:11:55 +0900

    [Config] update Config packages naming

---
+++ packages/atomic/src/components/card/Card.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+const styles = require('./Card.css');
+
+export interface CardProps {
+  children: React.ReactNode;
+  // IsNotRequired
+  className?: string;
+  style?: {
+    color?: string,
+    backgroundColor?: string,
+    padding?: string,
+    margin?: string,
+  };
+}
+
+const Card: React.StatelessComponent<CardProps> = ({
+  className,
+  children,
+  style,
+}) => (
+  <div className={`${styles.card} ${className}`} style={style}>
+    {children}
+  </div>
+);
+
+Card.defaultProps = {
+  className: '',
+  style: {
+    color: '',
+    backgroundColor: '',
+    padding: '',
+    margin: '',
+  },
+};
+
+export default Card;
---
+++ packages/atomic/src/components/hr/Hr.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+const styles = require('./Hr.css');
+
+export interface HrProps {
+  // isNotRequired
+  className?: string;
+  color?: string;
+  style?: {
+    margin?: string,
+  };
+}
+
+const Hr: React.SFC<HrProps> = ({
+  className = '',
+  color = '#000',
+  style = {
+    margin: '1rem 0',
+  },
+}) => (
+  <hr
+    className={`${styles.separator} ${className}`}
+    style={{
+      ...style,
+      borderTop: `1px solid ${color}`,
+    }}
+  />
+);
+
+export default Hr;
