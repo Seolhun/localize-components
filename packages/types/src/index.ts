58579b07aa1207c0fdfc4e9ac0a655f697dbd261
Seolhun <shun10114@gmail.com>
2018-09-29 16:25:53 +0900

    [Config] Update tpyes to use commonly

---
+++ packages/types/src/index.ts
@@ -0,0 +1,16 @@
+import { Color, ColorType, LightenColor, DarkenColor } from './color';
+import { Size, SizeType } from './size';
+import { Position, PositionType } from './position';
+
+export {
+  Color,
+  ColorType,
+  LightenColor,
+  DarkenColor,
+  // Size
+  Size,
+  SizeType,
+  // Position
+  Position,
+  PositionType,
+};
---
+++ packages/types/src/position.ts
@@ -0,0 +1,24 @@
+export const Position = {
+  BC: 'bottom-center',
+  BL: 'bottom-left',
+  BR: 'bottom-right',
+  CENTER: 'center',
+  LEFT: 'left',
+  RIGHT: 'right',
+  TC: 'top-center',
+  TL: 'top-left',
+  TR: 'top-right',
+};
+
+export type PositionType =
+  | 'bottom-center'
+  | 'bottom-left'
+  | 'bottom-right'
+  | 'center'
+  | 'left'
+  | 'right'
+  | 'top-center'
+  | 'top-left'
+  | 'top-right';
+
+export default Position;
