07c4382bd755aede140920da9fc1ca1fd0b4f568
Seolhun <shun10114@gmail.com>
2018-11-18 02:04:53 +0900

    [react] change import modules name

--- packages/localize-components-atomic/src/components/form/Input.tsx
+++ packages/localize-components-atomic/src/components/form/Input.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 import BoxInput from './input/BoxInput';
 import UnderLineInput from './input/UnderLineInput';
--- packages/localize-components/src/components/chip/Chip.tsx
+++ packages/localize-components/src/components/chip/Chip.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import * as classnames from 'classnames';
 
 import { LocalizeColorType } from '@seolhun/localize-components-types';
