1034cb2b966e14607a5be3cade63c580c8368f8d
Seolhun <shun10114@gmail.com>
2019-01-10 04:37:18 +0900

    [docz] upgrade docz version because of emotion

--- packages/docs/doczrc.js
+++ packages/docs/doczrc.js
@@ -1,14 +1,28 @@
 /**
  * @see https://www.docz.site/documentation/project-configuration
+ * @see https://github.com/pedronauck/docz/tree/master/packages/docz-theme-default
  */
 export default {
-  title: "Localzie Components Docs",
+  title: "Localzie Components",
   src: "./src",
   dest: "/dist",
   port: 8000,
   hashRouter: true,
   typescript: true,
   themeConfig: {
+    mode: 'dark',
+    htmlContext: {
+      head: {
+        links: [{
+          rel: 'stylesheet',
+          href: 'https://codemirror.net/theme/dracula.css'
+        }]
+      }
+    },
+    radii: '4px',
+    themeConfig: {
+      codemirrorTheme: 'dracula'
+    },
     showPlaygroundEditor: true,
     styles: {
       h1: {
