6953f7a9279df90dd9889c75917c9aa4f8c95ec5
Seolhun <shun10114@gmail.com>
2019-01-25 05:56:31 +0900

    [input] update input using emotion js

--- packages/localize-components-form/src/components/input/Input.tsx
+++ packages/localize-components-form/src/components/input/Input.tsx
@@ -1,19 +1,22 @@
 import React, { PureComponent, ReactNode } from 'react';
 
+import styled from '@emotion/styled';
+import { darken, lighten } from 'polished';
+
 import classnames from 'classnames';
 
 import {
-  BrandTheme,
-  ThemeStyle,
-  ThemeStyleType,
-  ThemeType,
+  FontSizes,
+  ThemeConfig,
+  Themes,
+  ThemesType,
+} from '@seolhun/localize-components-styled-types';
+import {
+  getIsLightenTheme,
+} from '@seolhun/localize-components-styled-utils';
+import {
   ValidationResponse,
 } from '@seolhun/localize-components-types';
-import {
-  getThemeStyle,
-} from '@seolhun/localize-components-utils';
-
-import './Input.css';
 
 export interface InputProps {
   // isRequired
@@ -100,15 +103,15 @@ export interface InputProps {
    */
   style?: {};
   /**
-   * Set this to change Input ours theme type
-   * @default 'background'
+   * Set this to change Button ours mainColor
+   * @default ThemeConfig.MAIN_THEME = royal_blue
    */
-  themeType?: ThemeStyleType;
+  mainColor?: ThemesType;
   /**
-   * Set this to change Input ours theme
-   * @default 'main'
+   * Set this to change Button ours subColor
+   * @default ThemeConfig.SUB_THEME = gray
    */
-  theme?: ThemeType;
+  subColor?: ThemesType;
   /**
    * Set this to change Input type
    * @default 'text'
@@ -128,7 +131,7 @@ export interface InputState {
   message: string;
 }
 
-class Input extends PureComponent<InputProps, InputState> {
+export class Input extends PureComponent<InputProps, InputState> {
   private inputBoxRef: any;
   private inputRef: any;
 
@@ -250,9 +253,9 @@ class Input extends PureComponent<InputProps, InputState> {
       placeholder = '',
       required = false,
       style = {},
-      themeType = ThemeStyle.Outline,
-      theme = BrandTheme.BRAND_MAIN,
       type = 'text',
+      mainColor = ThemeConfig.MAIN_THEME,
+      subColor = ThemeConfig.SUB_THEME,
     }: InputProps = this.props;
     const {
       hasError,
@@ -264,25 +267,26 @@ class Input extends PureComponent<InputProps, InputState> {
     return (
       <div
         className={classnames(
-          '__Localize',
+          'Localize__Input',
           className,
         )}
       >
-        <div
+        <StyledInputBox
           ref={this.inputBoxRef}
           className={classnames(
             'InputBox',
             {
               hasError,
               isFocused,
-              [getThemeStyle(theme, { themeType })]: isFocused,
             },
           )}
+          mainColor={mainColor}
+          subColor={subColor}
           style={{
             ...style,
           }}
         >
-          <input
+          <StyledInput
             ref={this.inputRef}
             disabled={disabled}
             onBlur={this.handleOnBlur}
@@ -296,9 +300,11 @@ class Input extends PureComponent<InputProps, InputState> {
             }}
             type={type}
             value={value}
+            mainColor={mainColor}
+            subColor={subColor}
           />
           {enterButton && (
-            <div
+            <StyledEnterButton
               className={classnames(
                 {
                   hasError,
@@ -309,10 +315,10 @@ class Input extends PureComponent<InputProps, InputState> {
               onClick={this.handleOnSubmit}
             >
               {enterButton}
-            </div>
+            </StyledEnterButton>
           )}
-        </div>
-        <div
+        </StyledInputBox>
+        <StyledErrorBox
           className={classnames(
             'ErrorBox',
             {
@@ -323,10 +329,162 @@ class Input extends PureComponent<InputProps, InputState> {
           {hasError && (
             `*${message}`
           )}
-        </div>
+        </StyledErrorBox>
       </div>
     );
   }
 }
+
+export interface InputBoxProps {
+  /**
+   * Set this to change Button ours mainColor
+   * @default ThemeConfig.MAIN_THEME = royal_blue
+   */
+  mainColor?: ThemesType;
+  /**
+   * Set this to change Button ours subColor
+   * @default ThemeConfig.SUB_THEME = gray
+   */
+  subColor?: ThemesType;
+}
+
+const StyledInputBox = styled.div<InputBoxProps>`
+  background-color: ${Themes.white};
+  border-radius: 3px;
+  border: 2px solid transparent;
+  display: flex;
+  height: 50px;
+  padding: 0;
+  transition: border-color 0.5s, background-color 0.5s;
+  vertical-align: middle;
+  width: 100%;
+
+  &:required {
+    color: ${Themes.black};
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+  }
+
+  &:hover {
+    border: 2px solid ${({
+      mainColor = ThemeConfig.MAIN_THEME,
+    }: InputBoxProps) => {
+      if (getIsLightenTheme(mainColor)) {
+        return darken(0.1, Themes[mainColor]);
+      }
+      return lighten(0.1, Themes[mainColor]);
+    }};
+  }
+
+  &.isFocused {
+    border: 2px solid ${({
+      mainColor = ThemeConfig.MAIN_THEME,
+    }: InputBoxProps) => {
+      return Themes[mainColor];
+    }};
+    color: ${({
+      mainColor = ThemeConfig.MAIN_THEME,
+    }: InputBoxProps) => {
+      if (getIsLightenTheme(mainColor)) {
+        return Themes.white;
+      }
+      return Themes.black;
+    }};
+  }
+`;
+
+const StyledInput = styled.input<InputProps>`
+    background-color: transparent;
+    border: 0;
+    color: : ${Themes.gray};
+    cursor: text;
+    display: block;
+    height: auto;
+    line-height: 25px;
+    margin: auto 10px;
+    outline: none;
+    padding: 0;
+    white-space: nowrap;
+    width: 100%;
+
+    &::placeholder {
+      font-size: 14px;
+      font-weight: 300;
+      font-style: normal;
+      font-stretch: normal;
+      line-height: 1.90;
+      letter-spacing: -0.3px;
+      color: ${({
+        mainColor = ThemeConfig.MAIN_THEME,
+      }: InputProps) => {
+        if (getIsLightenTheme(mainColor)) {
+          return Themes.white;
+        }
+        return Themes.black;
+      }};
+    }
+
+    &:required {
+      color: ${Themes.warning};
+    }
+
+    &:disabled {
+      background-color: ${Themes.basic};
+      border: 2px solid ${Themes.basic};
+      cursor: not-allowed !important;
+    }
+
+    &:hover {
+      color: ${Themes.black};
+    }
+  }
+`;
+
+const StyledEnterButton = styled.button`
+  align-items: center;
+  background-color: ${Themes.gray};
+  border-radius: 0 3px 3px 0;
+  color: ${Themes.white};
+  cursor: pointer;
+  display: inline-flex;
+  flex-basis: 50px;
+  flex-shrink: 0;
+  float: right;
+  height: 100%;
+  justify-content: center;
+  vertical-align: middle;
+  width: 50px;
+
+  &.isFilled {
+    background-color: ${Themes.gray};
+  }
+
+  &.hasError {
+    background-color: ${Themes.gray};
+    cursor: not-allowed !important;
+  }
+
+  &:hover, &:active {
+    background-color: ${Themes.gray};
+  }
+`;
+
+const StyledErrorBox = styled.div`
+  display: flex;
+  font-size: ${FontSizes.H1}
+  font-weight: 300;
+  height: auto;
+  min-height: 10px;
+  letter-spacing: -0.2px;
+  line-height: 0.90;
+  margin-top: 10px;
+  vertical-align: middle;
+  width: 100%;
+  &.hasError {
+    color: ${Themes.danger}
+  }
+`;
 
 export default Input;
