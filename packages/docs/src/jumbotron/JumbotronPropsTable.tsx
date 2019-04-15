b98fef9192658063eeb5e484a4eea4b2aacc08c3
Seolhun <shun10114@gmail.com>
2019-04-15 22:07:12 +0900

    [components] fix styled components properly to use props

---
+++ packages/docs/src/jumbotron/JumbotronPropsTable.tsx
@@ -0,0 +1,11 @@
+import React, { SFC } from 'react';
+
+import { JumbotronProps } from '@seolhun/localize-components';
+
+const JumbotronPropsTable: SFC<JumbotronProps> = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default JumbotronPropsTable;
