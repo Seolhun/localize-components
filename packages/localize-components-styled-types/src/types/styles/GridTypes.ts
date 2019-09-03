8abec948f9b1fad26c8f0790defdd31b5847c083
Seolhun <shun10114@gmail.com>
2019-09-04 02:09:10 +0900

    Recovery removed files in style types

---
+++ packages/localize-components-styled-types/src/types/styles/GridTypes.ts
@@ -0,0 +1,14 @@
+interface IResponsiveValue {
+  span: number
+  offset?: number
+}
+
+export type IColumnValue = number | IResponsiveValue
+
+export enum MediaQueries {
+  XL = 1200,
+  LG = 960,
+  MD = 768,
+  SM = 480,
+  XS = 0,
+}
