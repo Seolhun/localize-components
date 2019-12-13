85dbd42fd4de2c4c1b89dd2e0881d844a1a528f7
Seolhun <shun10114@gmail.com>
2019-12-14 01:01:19 +0900

    Bump up all packages version

---
+++ packages/docs/src/box/BoxPropsTable.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react';
+
+import { LocalizeJumbotronProps } from '@seolhun/localize-components';
+
+const JumbotronPropsTable: FunctionComponent<LocalizeJumbotronProps> = () => {
+  return <div />;
+};
+
+export default JumbotronPropsTable;
