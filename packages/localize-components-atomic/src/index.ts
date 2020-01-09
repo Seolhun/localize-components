aba47b936624cbaf4bcce0495ca3496135ff054a
Seolhun <shun10114@gmail.com>
2020-01-10 00:07:01 +0900

    remove index.d unused files

--- packages/localize-components-atomic/src/index.ts
+++ packages/localize-components-atomic/src/index.ts
@@ -1,6 +1,5 @@
-export * from '@seolhun/localize-components-button';
-export * from '@seolhun/localize-components-form';
-export * from '@seolhun/localize-components-icons';
+// export * from '@seolhun/localize-components-button';
+// export * from '@seolhun/localize-components-form';
+// export * from '@seolhun/localize-components-icons';
 
-// Components
 export * from './components'
