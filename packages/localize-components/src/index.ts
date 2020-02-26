29ca7f3bee5158508e8ddd12dd48bf739fc9f9dc
Seolhun <shun10114@gmail.com>
2020-02-26 20:33:38 +0900

    change all packages export from atomic to origin

--- packages/localize-components/src/index.ts
+++ packages/localize-components/src/index.ts
@@ -1,11 +1,13 @@
-export * from '@seolhun/localize-components-grid';
-export * from '@seolhun/localize-components-dropdown';
-export * from '@seolhun/localize-components-hooks';
 export {
   LocalizeTheme,
   LocalizeBaseStyledProps,
   LocalizeStyledProps,
   LocalizeThemeStyledProps,
 } from '@seolhun/localize-components-styled-types';
+
+export * from '@seolhun/localize-components-dropdown';
+export * from '@seolhun/localize-components-grid';
+export * from '@seolhun/localize-components-form';
+export * from '@seolhun/localize-components-icons';
 
 export * from './components';
