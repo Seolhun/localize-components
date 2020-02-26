abd4ec16467d9e24c020e0ba97e914aca9a577ef
Seolhun <shun10114@gmail.com>
2020-02-26 20:43:40 +0900

    fix export duplication modules

--- packages/localize-components/src/index.ts
+++ packages/localize-components/src/index.ts
@@ -1,13 +1,1 @@
-export {
-  LocalizeTheme,
-  LocalizeBaseStyledProps,
-  LocalizeStyledProps,
-  LocalizeThemeStyledProps,
-} from '@seolhun/localize-components-styled-types';
-
-export * from '@seolhun/localize-components-dropdown';
-export * from '@seolhun/localize-components-grid';
-export * from '@seolhun/localize-components-form';
-export * from '@seolhun/localize-components-icons';
-
 export * from './components';
