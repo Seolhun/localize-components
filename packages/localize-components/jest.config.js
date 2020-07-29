c25ceb500c59df2478781c6404d9b92552ed1142
Seolhun <shun10114@gmail.com>
2020-07-30 03:34:02 +0900

    fix tsconfig __test__ name to test

--- packages/localize-components/jest.config.js
+++ packages/localize-components/jest.config.js
@@ -25,7 +25,7 @@ module.exports = {
     '^.+\\.module\\.(css|sass|scss)$',
   ],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  testMatch: ['<rootDir>/__test__/**/*.(test|spec).ts?(x)'],
+  testMatch: ['<rootDir>/test/**/*.(test|spec).ts?(x)'],
   moduleFileExtensions: [
     ...defaults.moduleFileExtensions,
     'ts',
@@ -37,9 +37,9 @@ module.exports = {
     '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$':
       'identity-obj-proxy',
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-    '^.+\\.worker.(js|ts)$': '<rootDir>/__test__/__mocks__/workerMock.ts',
-    'worker-loader?(.*)': '<rootDir>/__test__/__mocks__/workerMock.ts',
+    '^.+\\.worker.(js|ts)$': '<rootDir>/test/__mocks__/workerMock.ts',
+    'worker-loader?(.*)': '<rootDir>/test/__mocks__/workerMock.ts',
   },
   setupFiles: ['jest-canvas-mock'],
-  setupFilesAfterEnv: ['<rootDir>/__test__/setUpTest.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test/setUpTest.ts'],
 };
