87640d74374e21b01f65cec9d823ea8ca81b3842
Seolhun <shun10114@gmail.com>
2020-11-21 01:16:27 +0900

    feat : update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,36 @@ module.exports = {
     jest: true,
     browser: true,
   },
-  extends: ['airbnb-typescript', 'airbnb/hooks', 'plugin:@typescript-eslint/eslint-recommended'],
+  extends: ['plugin:@typescript-eslint/eslint-recommended', 'plugin:prettier/recommended'],
   plugins: ['@typescript-eslint', 'eslint-plugin', 'jest'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
   rules: {
     // TODO: Will be Removed
     'import/no-cycle': 0,
     'mouse-events-have-key-events': 0,
+    // Nestjs Error
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': 0,
+    'import/no-duplicates': 0,
+    'import/no-self-import': 0,
+    'import/order': 0,
+    'import/no-useless-path-segments': 0,
     // JSX
     'jsx-a11y/mouse-events-have-key-events': 0,
     'jsx-a11y/anchor-is-valid': 0,
     // React
     'react/prop-types': 0,
-    'react-hooks/exhaustive-deps': 0,
     'react/no-array-index-key': 0,
     'react/prop-types': 0, // typescript 충돌
     'react/jsx-props-no-spreading': 0,
-    'react/jsx-wrap-multilines': ['error', { arrow: true, return: true, declaration: true }],
   },
   settings: {
     'prettier/prettier': 'error',
