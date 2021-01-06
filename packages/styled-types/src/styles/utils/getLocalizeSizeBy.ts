03c21d96ab4228989b937aa9872496b359e2081c
Seolhun <shun10114@gmail.com>
2021-01-06 11:50:24 +0900

    feat : change localize size scale

--- packages/styled-types/src/styles/utils/getLocalizeSizeBy.ts
+++ packages/styled-types/src/styles/utils/getLocalizeSizeBy.ts
@@ -3,23 +3,23 @@ import { LocalizeSize } from '../LocalizeSize';
 export const getLocalizeSizeBy = (size?: LocalizeSize) => {
   switch (size) {
     case 'xl': {
-      return '1.8rem';
-    }
-    case 'lg': {
       return '1.6rem';
     }
-    case 'md': {
-      return '1.2rem';
+    case 'lg': {
+      return '1.4rem';
     }
-    case 'sm': {
+    case 'md': {
       return '1rem';
     }
-    case 'xs': {
+    case 'sm': {
       return '0.8rem';
+    }
+    case 'xs': {
+      return '0.6rem';
     }
     // MD
     default: {
-      return '1.2rem';
+      return '1rem';
     }
   }
 };
