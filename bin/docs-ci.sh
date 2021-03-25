3a34bdfd7a22c6b7986485f25f7eb92d6f614b9a
Seolhun <shun10114@gmail.com>
2021-03-26 03:03:04 +0900

    chore : update workflows with surge

--- bin/docs-ci.sh
+++ bin/docs-ci.sh
@@ -1,14 +1,10 @@
-if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]; then
-    cd docs
-    npm install
-    npm run build
+cd docs
+npm install
+npm run build
 
-    if [ $TRAVIS_BRANCH = 'master' ]; then
-      surge dist/ http://localize-components-docs.surge.sh
-    else
-      # Develop
-      surge dist/ http://dev.localize-components-docs.surge.sh
-    fi
-  fi
+if [ $TRAVIS_BRANCH = 'master' ]; then
+  surge dist/ http://localize-components-docs.surge.sh --token ${{secrets.SURGE_TOKEN}}
+else
+  # Develop
+  surge dist/ http://dev.localize-components-docs.surge.sh --token ${{secrets.SURGE_TOKEN}}
 fi
