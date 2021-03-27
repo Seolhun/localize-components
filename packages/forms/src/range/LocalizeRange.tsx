43541eba4fd5127f2cfef275bf1a70bdcca683b7
Seolhun <shun10114@gmail.com>
2021-03-28 01:37:14 +0900

    feat : update localize range with stories

--- packages/forms/src/range/LocalizeRange.tsx
+++ packages/forms/src/range/LocalizeRange.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import classnames from 'classnames';
+// import { transparentize } from 'polished';
 
 import {
   LocalizeProps,
@@ -50,19 +51,7 @@ export interface LocalizeRangeProps extends ExtentionProps {
   vertical?: boolean;
 }
 
-interface LocalizeRangeWrapperProps extends LocalizeRangeProps {
-  /**
-   * Set this to change tracker width
-   */
-  trackerWidth: number;
-
-  /**
-   * Set this to change handler width on tracker
-   */
-  handlerWidth: number;
-}
-
-const LocalizeRangeWrapper = styled.div<LocalizeRangeWrapperProps, LocalizeThemeProps>(
+const LocalizeRangeWrapper = styled.div<LocalizeRangeProps, LocalizeThemeProps>(
   ({
     theme,
     scale = 'md',
@@ -74,157 +63,93 @@ const LocalizeRangeWrapper = styled.div<LocalizeRangeWrapperProps, LocalizeTheme
       inversedFontColor: 'inversed10',
     },
     rounded,
-    trackerWidth,
-    handlerWidth,
   }) => {
     const localizedColor = getLocalizeIntentColor(theme, intent, localize);
     const { primaryColor, neutralColor, inversedFontColor } = localizedColor;
     const localizeScale = getLocalizeScaleBy(scale);
 
+    const track: any = {
+      content: ' ',
+      background: 'transparent',
+      borderRadius: '6px',
+      pointerEvents: 'none',
+    };
+    const thumb: any = {
+      height: `${localizeScale}rem`,
+      width: `${localizeScale}rem`,
+      background: inversedFontColor,
+      border: `1px solid ${neutralColor}`, //$thumb-border,
+      borderRadius: rounded ? '50%' : '6px',
+    };
+
     return {
       position: 'relative',
-      display: 'flex',
-      alignItems: 'center',
-      color: inversedFontColor,
+      appearance: 'none',
       outline: 'none',
-      cursor: 'pointer',
       userSelect: 'none',
+      cursor: 'pointer',
 
-      // Hover
-      '&:hover': {
-        [`${HidingInput}:not(:disabled):not(:read-only):not(:checked) + .${CLASSNAME}__Tracker`]: {
-          borderColor: primaryColor,
+      '&:after': {
+        content: ' ',
+        position: 'absolute',
+        top: 0,
+        width: 0,
+        background: primaryColor,
+        height: '8px',
+        left: '1px',
+        zIndex: 1,
+      },
+      'input': {
+        WebkitAppearance: 'none',
+        MozAppearance: 'none',
+        '&:focus': {
+          boxShadow: 'none',
+          outline: 'none',
         },
-      },
-
-      // Readonly - Disabled
-      [`${HidingInput}:read-only, ${HidingInput}:disabled`]: {
-        backgroundColor: theme.colors.disabled,
-        borderColor: theme.colors.neutral5,
-      },
-
-      /**
-       * @name __Tracker
-       */
-      [`.${CLASSNAME}__Tracker`]: {
-        width: '100%',
-        height: `${localizeScale / 2}rem`,
-        backgroundColor: neutralColor,
-        borderRadius: '4px',
-
-        '&:after': {
-          width: `${handlerWidth / trackerWidth}%`,
-          height: `${localizeScale / 2}rem`,
-          backgroundColor: primaryColor,
-          borderRadius: '4px',
-        },
-      },
-
-      // Active
-      [`${HidingInput}:not(:disabled):active + .${CLASSNAME}__Tracker`]: {
-        backgroundColor: theme.colors.inversed1,
-        borderColor: primaryColor,
-      },
-
-      // Disabled and Checked
-      [`${HidingInput}:disabled:checked + .${CLASSNAME}__Tracker`]: {
-        backgroundColor: theme.colors.disabled,
-        borderColor: theme.colors.neutral5,
-      },
-
-      /**
-       * @name __Handler
-       */
-      [`.${CLASSNAME}__Handler`]: {
-        width: `${localizeScale}rem`,
-        height: `${localizeScale}rem`,
-        borderRadius: rounded ? '50%' : '6px',
-        border: `1px solid ${theme.colors.neutral6}`,
-        backgroundColor: theme.colors.inversed1,
-        cursor: 'grabbing',
-      },
-
-      // Active
-      [`${HidingInput}:not(:disabled):active + .${CLASSNAME}__Handler`]: {
-        backgroundColor: theme.colors.inversed1,
-        borderColor: primaryColor,
-      },
-
-      // Disabled and Checked
-      [`${HidingInput}:disabled:checked + .${CLASSNAME}__Handler`]: {
-        backgroundColor: theme.colors.disabled,
-        borderColor: theme.colors.neutral5,
+        /**
+         * @name Track
+         */
+        '&::-webkit-slider-runnable-track': track,
+        '&::-ms-track': track,
+        '&::-moz-range-track': track,
+        /**
+          * @name Thumb
+          */
+        '&::-webkit-slider-thumb': thumb,
+        '&::-ms-thumb': thumb,
+        '&::-moz-range-thumb': thumb,
       },
     };
   },
 );
 
-const HidingInput = styled.input<{}, LocalizeThemeProps>(({ theme }) => {
-  return {
-    appearance: 'none',
-    width: '100%',
-    height: '5px',
-    padding: 0,
-    margin: 0,
-    borderRadius: '2px',
-    outline: 'none',
-
-    // Range Handler
-    '&::-webkit-slider-thumb': {
-      appearance: 'none',
-      width: '24px',
-      height: '24px',
-      borderRadius: '12px',
-      background: theme.colors.secondary,
-      cursor: 'pointer',
-    },
-    '&::-moz-range-thumb': {
-      appearance: 'none',
-      width: '24px',
-      height: '24px',
-      borderRadius: '12px',
-      background: theme.colors.secondary,
-      cursor: 'pointer',
-    },
-    '&:focus': {
-      '&::-webkit-slider-thumb': {
-        backgroundColor: theme.colors.neutral8,
-      },
-      '&::-moz-range-thumb': {
-        backgroundColor: theme.colors.neutral8,
-      },
-    },
-    '&:disabled': {
-      backgroundColor: theme.colors.neutral5,
-      '&::-webkit-slider-thumb': {
-        backgroundColor: theme.colors.neutral5,
-      },
-      '&::-moz-range-thumb': {
-        backgroundColor: theme.colors.neutral5,
-      },
-    },
-  };
-});
-
-const LocalizeRangeTracker = styled.div<{}, LocalizeThemeProps>(() => {
+const LocalizeRangeContainer = styled.div<{}, LocalizeThemeProps>(() => {
   return {
     display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
-    width: '100%',
   };
 });
 
-const LocalizeHandler = styled.div<{}, LocalizeThemeProps>(() => {
+const LocalizeRangeInput = styled.input<{}, LocalizeThemeProps>(() => {
   return {
-    display: 'inline-block',
+    appearance: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    background: 'none',
+    height: '100%',
+    width: '100%',
+    overflow: 'hidden',
   };
 });
 
-const LocalizeRangeLabelContainer = styled.div<{}, LocalizeThemeProps>(() => {
+const LocalizeRangeLabelWrapper = styled.div<{}, LocalizeThemeProps>(() => {
   return {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
+    width: '100%',
+    marginTop: '8px',
   };
 });
 
@@ -249,56 +174,7 @@ const LocalizeRange = React.forwardRef<HTMLInputElement, LocalizeRangeProps>(
     },
     ref,
   ) => {
-    const { value, defaultValue, min, max } = props;
-    const initValue = value || defaultValue || 0;
-
-    const memoizedInitValue = React.useMemo<number>(() => {
-      const parsedValue = Number(value);
-      const parseDefaultdValue = Number(defaultValue);
-      const initValue = parsedValue || parseDefaultdValue || 0;
-      if (Number.isInteger(initValue)) {
-        return initValue;
-      }
-      return 0;
-    }, [initValue]);
-
-    const memoizedMinValue = React.useMemo<number>(() => {
-      const parsedMinValue = Number(min);
-      if (Number.isInteger(parsedMinValue)) {
-        return parsedMinValue;
-      }
-      return 0;
-    }, [min]);
-
-    const memoizedMaxValue = React.useMemo<number>(() => {
-      const parsedMaxValue = Number(max);
-      if (Number.isInteger(parsedMaxValue)) {
-        return parsedMaxValue;
-      }
-      return 100;
-    }, [max]);
-
-    const handlerValue = memoizedInitValue - memoizedMinValue;
-    const maxValue = memoizedMaxValue - memoizedMinValue;
-    const [trackerWidth, setTrackerWidth] = React.useState<number>(handlerValue / maxValue);
-    const [handlerWidth, setHandlerWidth] = React.useState<number>(handlerValue);
-
-    const handleTrackerWidth = React.useCallback((value: number) => {
-      setTrackerWidth(value);
-    }, []);
-
-    const handleHandlerWidth = React.useCallback((value: number) => {
-      setHandlerWidth(value);
-    }, []);
-
-    React.useEffect(() => {
-      handleTrackerWidth(handlerValue / maxValue);
-    }, [handlerValue, maxValue]);
-
-    React.useEffect(() => {
-      handleHandlerWidth(handlerValue);
-    }, [handlerValue]);
-
+    const { min, max } = props;
     return (
       <LocalizeRangeWrapper
         {...props}
@@ -307,17 +183,14 @@ const LocalizeRange = React.forwardRef<HTMLInputElement, LocalizeRangeProps>(
         intent={intent}
         scale={scale}
         rounded={rounded}
-        trackerWidth={trackerWidth}
-        handlerWidth={handlerWidth}
       >
-        <HidingInput {...props} ref={ref} type="range" />
-        <LocalizeRangeTracker className={`${CLASSNAME}__Tracker`}>
-          <LocalizeHandler className={`${CLASSNAME}__Handler`} />
-        </LocalizeRangeTracker>
-        <LocalizeRangeLabelContainer className={`${CLASSNAME}__Label`}>
-          <LocalizeRangeLabel className={`${CLASSNAME}__Label__Min`}>{min}</LocalizeRangeLabel>
-          <LocalizeRangeLabel className={`${CLASSNAME}__Label__Max`}>{max}</LocalizeRangeLabel>
-        </LocalizeRangeLabelContainer>
+        <LocalizeRangeContainer>
+          <LocalizeRangeInput {...props} ref={ref} type="range" />
+          <LocalizeRangeLabelWrapper className={`${CLASSNAME}__Label`}>
+            <LocalizeRangeLabel className={`${CLASSNAME}__Label__Min`}>{min}</LocalizeRangeLabel>
+            <LocalizeRangeLabel className={`${CLASSNAME}__Label__Max`}>{max}</LocalizeRangeLabel>
+          </LocalizeRangeLabelWrapper>
+        </LocalizeRangeContainer>
       </LocalizeRangeWrapper>
     );
   },
